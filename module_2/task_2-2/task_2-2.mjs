ecd3932a5fcda18c3a5eb4e0d7213683c2e22e0d
QuandaleDingle-Wiz <soewisdom@gmail.com>
2024-10-07 16:03:42 +0200

    task_2-2

--- module_2/task_2-2/task_2-2.mjs
+++ module_2/task_2-2/task_2-2.mjs
@@ -16,7 +16,7 @@ printOut("2 + (3*(2-4) * 6 = " + Solution);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/*Convert 25 metres and 34 centimetres to inches. An inch is 25.4 millimetres (maximum 2 decimal places in
+/*Convert 25 metres and 34 centimeters to inches. An inch is 25.4 millimeters (maximum 2 decimal places in
 the answer).*/
 const MillInInches = 25.4;
 const MillInMetres = 1000;
@@ -44,12 +44,41 @@ printOut(newLine);
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /*Convert 6,322.52 minutes to days, hours, minutes, and seconds. (Not allowed to use date objects). The
 task must be solved with primitives.*/
-printOut("Replace this with you answer!");
+const NumberOfMinutes = 6322.52;
+const SecondsInMinutes = 60;
+const MinutesInHours = 60;
+const HoursInDays = 24; 
+let Solution4_Calc = NumberOfMinutes / (60 * 24);
+const Solution4_Days = Math.floor(Solution4_Calc);
+printOut("Days = " + Solution4_Days);
+
+Solution4_Calc = Solution4_Calc - Solution4_Days;
+Solution4_Calc = Solution4_Calc * HoursInDays;
+const Solution4_Hours = Math.floor(Solution4_Calc);
+printOut("Hours = " + Solution4_Hours);
+
+Solution4_Calc = Solution4_Calc - Solution4_Hours;
+Solution4_Calc = Solution4_Calc * MinutesInHours;
+const Solution4_Minutes = Math.floor(Solution4_Calc);
+printOut("Minutes = " + Solution4_Minutes);
+
+Solution4_Calc = Solution4_Calc - Solution4_Minutes;
+Solution4_Calc = Solution4_Calc * SecondsInMinutes;
+const Solution4_Seconds = Math.floor(Solution4_Calc);
+printOut("Seconds = " + Solution4_Seconds);
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/*Convert 54 dollars to Norwegian kroner, and print the price for both:
+NOK → USD and USD → NOK.
+Use 76 NOK = 8.6 USD as the exchange rate.
+The answer must be in whole "Kroner" and whole "dollars".*/
+const NOK = 76 / 8.6;
+const USD = 8.6 / 76;
+let AmountUSD = 54;
+const AmountNOK = Math.round(AmountUSD * NOK);
+printOut(AmountUSD + " dollars is " + AmountNOK + " kroner ");
+printOut(AmountNOK + " kroner is " + AmountUSD + " dollar ");
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
@@ -66,27 +95,57 @@ const earthIndex = solution6Text.indexOf("earth");
 printOut(solution6Text);
 printOut("Antall bokstaver i teksten: " + count.toString());
 printOut("Karakteren i posisjon 19 er: " + charPos);
-printOut("Karakteren mellom 35 og 8 er: ")
-
+printOut("Karakteren mellom 35 og 8 er: " + charBetween);
+printOut("Index for earth er: " + earthIndex)
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/*Comparison, print the values for the following expressions (evaluate whether the statements are true*/
+let s7a = 5 > 3;
+let s7b = 7 >= 7;
+let s7c = "a" > "b";
+let s7d = 1 < "a";
+let s7e = "2500" < "abcd";
+let s7f = "arne" !== "thomas";
+let s7g = (2 === 5) === true;
+let s7h = ("abcd" > "bcd") === false;
+
+printOut("5 > 3 is " + s7a.toString());
+printOut("7 >= 7 is " + s7b.toString());
+printOut("a > b is " + s7c.toString());
+printOut("1 < a is " + s7d.toString());
+printOut("2500 < abcd is " + s7e.toString());
+printOut("arne !== thomas is " + s7f.toString());
+printOut("(2 === 5) === true " + s7g.toString());
+printOut("(abcd > bcd) === false " + s7h.toString());
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/*Convert and print the following expressions*/
+let solution8ptA = "254";
+let solution8ptB = "57.23";
+let solution8ptC = "25 kroner";
+printOut("254 = " + Number(solution8ptA));
+printOut("57.23 = " + Number(solution8ptB));
+printOut("25 kroner = " + parseInt(solution8ptC));
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/*Create a variable "r" and randomly generate a number from 1 to 360 (1 >= r <= 360)*/
+let r = (Math.random() * 360) + 1;
+
+printOut("(math.random() * 360) + 1 = " + r.toFixed());
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/*Use modulus (%) to calculate how many weeks and days are in 131 days*/
+let totalDays = 131;
+
+const weeks = Math.floor(totalDays / 7);
+
+const days = totalDays % 7;
+
+printOut("Weeks = " + Number(weeks));
+printOut("days = " + Number(days));
 printOut(newLine);
